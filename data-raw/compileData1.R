71a001f26e4ddf0ee196b3063fb686f6b17ce849
Griesbauer <Hardy.Griesbauer@gov.bc.ca>
2020-02-06 15:45:16 -0800

    Compute Plot basal area

--- data-raw/compileData1.R
+++ data-raw/compileData1.R
@@ -14,6 +14,13 @@ siteData <-
   read_csv("data-raw/Site_data_raw.csv") %>% 
   mutate_each_(funs(factor(.)),vars(c("Opening","Plot","BGC.unit","SMR","SNR","Site.series"))) # convert first two columns to factors
 
+# Calculate plot basal area
+PlotBA<-
+  dat %>% 
+  filter(Crown.Status!=4) %>% # remove all suppressed trees from basal area calculation
+  group_by(Opening,Plot) %>% 
+  summarise(PlotBA=sum(DBH)*200*0.0001)   # multiply by 200 to get per hectare basal area
+
 
 # Concatenate data into 1 data set
 dat<-
@@ -21,9 +28,18 @@ dat<-
   left_join(siteData,by=c("Opening","Plot")) %>% 
 
 # Bring in planting year
-  left_join(read_csv("data-raw/plantingYear.csv"),by="Opening")
+  left_join(read_csv("data-raw/plantingYear.csv"),by="Opening") %>% 
   
+# rename column 'FG/C/C' 
+  dplyr::select(1:7,Crown.Status=`FG/C/C`,everything()) %>% 
   
+# Join with PlotBA
+  left_join(PlotBA,by=c("Opening","Plot"))
+  
+
+  
+
+
 # Save in data-raw folder
 # Use data screening to fix/format/remove outliers, etc.. and save into 'data' folder
 save(dat,file="data-raw/dat.RData")
---
+++ data-raw/computePlotBA.R
@@ -0,0 +1,5 @@
+# script to compute plot basal area and add to 'dat' file
+
+library(tidyverse)
+
+load("data")
